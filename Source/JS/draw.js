1542eacdb05f72555b7b68d5bca898ad28f67002
dxs4043 <dxs4043@IGM2000-24>
2018-10-05 15:32:29 -0400

    updated comments in draw.js

--- Source/JS/draw.js
+++ Source/JS/draw.js
@@ -1,5 +1,12 @@
+/*
+====================================================================================
+DRAW.JS
+-> DENNIS CUSTOM DRAWING FRAMEWORK
+-> Abastract away the canvas API draw calls as well as simplify some of the operations for drawing lines
+-> Holds helper functions for drawing the curves, gradient, etc effects
+====================================================================================
+*/
 export default class Draw {
-  //needs more code for global compositing
   constructor(ctx) {
     this.ctx = ctx;
   }
@@ -18,22 +25,27 @@ export default class Draw {
   translate(x = 0, y = 0) {
     this.ctx.translate(x, y);
   }
+  //rotate according to the supplied degrees
   rotate(degrees = 0) {
     this.ctx.rotate((degrees * Math.PI) / 180);
   }
   drawRect(x = 0, y = 0, width = 0, height = 0) {
     this.ctx.rect(x, y, width, height);
   }
+  //Begin the new line and move in immediately at the origin point. Saves the neccessity to write those two lines separately
   newL(x = 0, y = 0) {
     this.ctx.beginPath();
     this.ctx.moveTo(x, y);
   }
+  //Open the drawing path
   open() {
     this.ctx.beginPath();
   }
+  //Enclose the drawing path
   close() {
     this.ctx.closePath();
   }
+  //build lines and shapes with this
   toVertex(x, y) {
     this.ctx.lineTo(x, y);
   }
