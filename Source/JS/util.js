baf5c17d1dfeee348ae15c20b188fbd2d57b7f8e
dxs4043 <dxs4043@IGM2000-24>
2018-10-05 15:40:48 -0400

    updated comments in util.js

--- Source/JS/util.js
+++ Source/JS/util.js
@@ -1,11 +1,20 @@
+/*
+====================================================================================
+UTIL.JS
+-> Collection of helper that are used across many modules
+-> Examples include functions for creating color data, screen Clear, canvas setup, etc
+====================================================================================
+*/
+//return a string representing the rgba color values
 let makeColor = (red, green, blue, alpha = 1.0) => {
   let color = "rgba(" + red + "," + green + "," + blue + ", " + alpha + ")";
   return color;
 };
+//clear the screen for refresh of the animations
 let clearScreen = ctx => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
-
+//resize the canvas to fit full screen and hide the UI. Make sure to account for all browsers
 let requestFullscreen = element => {
   if (element.requestFullscreen) {
     element.requestFullscreen();
@@ -17,12 +26,13 @@ let requestFullscreen = element => {
     element.webkitRequestFullscreen();
   }
 };
+//return the generated ctx
 let setupCanvasData = (canvas, width, height) => {
   canvas.width = width;
   canvas.height = height;
   return canvas.getContext("2d");
 };
-
+//Return randomized color for image effects
 let getRandomColor = () => {
   const getByte = _ => 55 + Math.round(Math.random() * 200);
   let values = new Array(3);
@@ -31,7 +41,6 @@ let getRandomColor = () => {
   values[2] = getByte();
   return values;
 };
-
 export {
   makeColor,
   clearScreen,
